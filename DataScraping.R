28ca9c0b5cdd2179adc00426a006c53ebf4b7820
evgeniou <michail.evgeniou@gmail.com>
2023-12-04 12:36:25 +0100

    Initiation

---
+++ DataScraping.R
@@ -0,0 +1,4 @@
+source("https://raw.githubusercontent.com/MichailEvgeniou/BioinformaticTools/main/0.dependancies.R")
+source("https://raw.githubusercontent.com/MichailEvgeniou/BioinformaticTools/main/CraeteSymetricalMatric.R")
+
+print("helllo world")
